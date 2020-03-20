c1d908ed3d421f26ba9f1fd7a6810ee6571cfed6
SirDark <gergelykevin123@gmail.com>
2020-03-20 23:04:16 +0100

    started_map

---
+++ IslandClimb/Assets/SirDark/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 0.123f;
+    public Vector3 offset;
+
+
+    void FixedUpdate()
+    {
+        Vector3 desiredposition = target.position + offset;
+        desiredposition.y = Mathf.Clamp(desiredposition.y,0,9999);
+        Vector3 smoothed = Vector3.Lerp(transform.position,desiredposition, speed);
+        transform.position = smoothed;
+
+        //transform.LookAt(target);
+    }
+}
