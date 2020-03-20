19e19ded3e6fa0c8df451d8d9fe0218d249574b3
SirDark <gergelykevin123@gmail.com>
2020-03-20 14:11:06 +0100

    movement with rigidbody

--- IslandClimb/Assets/SirDark/movement.cs
+++ IslandClimb/Assets/SirDark/movement.cs
@@ -4,38 +4,63 @@ using System.Collections;
 
 public class movement : MonoBehaviour
 {
-    CharacterController characterController;
+    Rigidbody2D body;
 
     public float speed = 6.0f;
     public float jumpSpeed = 8.0f;
-    public float gravity = 20.0f;
+    //public float gravity = 20.0f;
 
-    private Vector3 moveDirection = Vector3.zero;
+    private float moveInput = 0f;
+
+    private bool isGrounded = true;
+    private bool onWall= false;
+
+    public string groundtag = "Ground";
+    public string walltag = "Wall";
+
+    
 
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        body = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
-    {
-        if (characterController.isGrounded)
-        {
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Space) && (isGrounded || onWall)){
             
+            if(isGrounded){
+                body.velocity = new Vector2(0,1)* jumpSpeed;
+                
 
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-            moveDirection *= speed;
-
-            if (Input.GetButton("Jump"))
-            {
-                moveDirection.y = jumpSpeed;
             }
+            else if(onWall){
+                body.velocity = new Vector2(1,1)* jumpSpeed;
+            }
+
         }
+        Debug.Log(isGrounded);
+    }
+    void FixedUpdate()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
 
-        
-        moveDirection.y -= gravity * Time.deltaTime;
+        body.velocity = new Vector2(moveInput*speed, body.velocity.y);
 
-        
-        characterController.Move(moveDirection * Time.deltaTime);
+    }
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == groundtag){
+            isGrounded = true;
+        }
+        if(other.gameObject.tag == walltag){
+            onWall = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D other) {
+        if(other.gameObject.tag == groundtag){
+            isGrounded = false;
+        }
+        if(other.gameObject.tag == walltag){
+            onWall = false;
+        }
     }
 }
